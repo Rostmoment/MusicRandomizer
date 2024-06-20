00f91d7f2420097e74e8332f4b3648f0e5c9118e
Rostmoment <110627353+Rostmoment@users.noreply.github.com>
2024-06-21 00:04:21 +0300

    Add files via upload

---
+++ RandomMusic/BasePlugin.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BepInEx;
+using HarmonyLib;
+using MTM101BaldAPI;
+using MTM101BaldAPI.AssetTools;
+using UnityEngine;
+
+namespace RandomMusic
+{
+	[HarmonyPatch(typeof(MusicManager))]
+	internal class MidiPlayerPatch
+	{
+		[HarmonyPatch("PlayMidi")]
+		[HarmonyPrefix]
+		private static void PlayMidiPatch(ref string song)
+        {
+			if (Directory.Exists(BasePlugin.path + song))
+			{
+				if (!CustomSongData.Exists(song))
+				{
+					CustomSongData.CreateSongData(song);
+				}
+				CustomSongData songData = CustomSongData.GetByName(song);
+				try
+				{
+					if (songData.NewDatas.Length > 0)
+					{
+						int id = 0;
+						if (songData.NewDatas.Length > 1)
+						{
+							id = UnityEngine.Random.Range(0, songData.NewDatas.Length - 1);
+						}
+						song = songData.NewDatas[id];
+					}
+				}
+				catch (NullReferenceException) { }
+			}
+			else
+			{
+				Directory.CreateDirectory(BasePlugin.path + song);
+			}
+		}
+    }
+
+	[BepInPlugin("rost.moment.baldiplus.randommusic", "Random Music","1.0")]
+	public class BasePlugin : BaseUnityPlugin
+	{
+		void Awake()
+		{
+			Harmony harmony = new Harmony("rost.moment.baldiplus.randommusic");
+			harmony.PatchAll();
+			Instance = this;
+			path = AssetLoader.GetModPath(this);
+			if (!Directory.Exists(path))
+			{
+				Directory.CreateDirectory(path);
+			}
+			path += "\\";
+        }
+		public static string path;
+		public static BasePlugin Instance;
+        public static List<CustomSongData> midis = new List<CustomSongData>() { };
+    }
+}
---
+++ RandomMusic/CustomSongData.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using MTM101BaldAPI.AssetTools;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using UnityEngine.Profiling.Memory.Experimental;
+
+namespace RandomMusic
+{
+    public class CustomSongData
+    {
+        public string OriginalName { get; set; }
+        public string[] NewDatas { get; set; }
+        public static bool Exists(string name)
+        {
+            return BasePlugin.midis.Exists(element => element.OriginalName == name);
+        }
+        public static bool CanBeUsed(string name)
+        {
+            if (!Exists(name)) return false;
+            if (GetByName(name).NewDatas.Length == 0) return false;
+            return true;
+        }
+        public static CustomSongData GetByName(string name)
+        {
+            return BasePlugin.midis.Find(element => element.OriginalName == name);
+        }
+        public static void CreateSongData(string songName)
+        {
+            string[] names = Directory.GetFiles(BasePlugin.path + songName);
+            if (names.Length == 0)
+            {
+                UnityEngine.Debug.Log("Folder " + songName + " is empty!");
+                return;
+            }
+            string[] data = new string[] { };
+            foreach (string name in names)
+            {
+                if (name.EndsWith(".mid"))
+                {
+                    UnityEngine.Debug.Log(name);
+                    data = data.AddToArray(AssetLoader.MidiFromFile(name, Path.GetFileNameWithoutExtension(name)));
+                }
+            }
+            BasePlugin.midis.Add(new CustomSongData
+            {
+                OriginalName = songName,
+                NewDatas = data
+            });
+        }
+    }
+}
